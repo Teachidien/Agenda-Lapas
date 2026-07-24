c7080b1075fe4a7c6f0a7be95bb5c9b112e51cb5
Admin <admin@sinora.internal>
2026-07-24 19:34:44 +0700

    Add fallback configs and update workflow env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBYpKQmRMlbASYnRP92UuylmaNDuj-jREM",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "agenda-lapas.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "agenda-lapas",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "agenda-lapas.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "897361130409",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:897361130409:web:e3f716135f0948d4a40ab8",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-ZQJP27W97T"
 };
 
 // Initialize Firebase
